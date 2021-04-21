b533d78e89e3ae1019d0f3cac10202b09a88e279
akalajzi <akalajzi@gmail.com>
2021-04-21 21:28:44 +0200

    change company details

--- src/components/Exchange.svelte
+++ src/components/Exchange.svelte
@@ -1,34 +1,36 @@
 <script>
-  import { onMount } from 'svelte';
+  import { onMount } from "svelte";
 
-  import ExchangeChart from './ExchangeChart.svelte';
-  import ExchangeTable from './ExchangeTable.svelte';
-  import { rates } from '../store';
-  import { subDays, toHnbDateFormat } from '../utils';
+  import ExchangeChart from "./ExchangeChart.svelte";
+  import ExchangeTable from "./ExchangeTable.svelte";
+  import { rates } from "../store";
+  import { subDays, toHnbDateFormat } from "../utils";
 
   function fetchRates(from, to) {
     if (!$rates.results) {
       rates.set({
         ...$rates,
         loading: true,
-      })
+      });
 
-      fetch(`https://cors-anywhere.herokuapp.com/api.hnb.hr/tecajn/v2?valuta=USD&datum-primjene-od=${from}&datum-primjene-do=${to}`)
-      .then(res => res.json())
-      .then(res => {
-        rates.set({
-          loading: false,
-          error: null,
-          results: res.reverse(),
+      fetch(
+        `https://cors-anywhere.herokuapp.com/api.hnb.hr/tecajn/v2?valuta=USD&datum-primjene-od=${from}&datum-primjene-do=${to}`
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          rates.set({
+            loading: false,
+            error: null,
+            results: res.reverse(),
+          });
         })
-      })
-      .catch(err => {
-        rates.set({
-          loading: false,
-          error: err,
-          results: null,
-        })
-      })
+        .catch((err) => {
+          rates.set({
+            loading: false,
+            error: err,
+            results: null,
+          });
+        });
     }
   }
 
@@ -39,22 +41,22 @@
     const to = toHnbDateFormat(now);
 
     fetchRates(from, to);
-  })
+  });
 </script>
+
+<div id="chart" style="position: relative" />
+
+{#if $rates.loading}
+  <div>calling HNB...</div>
+{:else if $rates.error}
+  <div>HNB is down, try again later.</div>
+{:else if $rates.results}
+  <ExchangeTable data={$rates.results} />
+  <ExchangeChart data={$rates.results} />
+{/if}
 
 <style>
   #chart {
     margin-bottom: 20px;
   }
 </style>
-
-<div id='chart' style="position: relative"></div>
-
-{#if $rates.loading }
-<div>calling HNB...</div>
-{:else if $rates.error}
-<div>{$rates.error}</div>
-{:else if $rates.results}
-<ExchangeTable data={$rates.results} />
-<ExchangeChart data={$rates.results} />
-{/if}
